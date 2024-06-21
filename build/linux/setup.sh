b20bef1c020cfbbc689a649a1e2e2a2e4a48ce02
aluminum-ice <12374267+aluminum-ice@users.noreply.github.com>
2024-06-21 13:32:05 -0700

    Update setup.sh

--- build/linux/setup.sh
+++ build/linux/setup.sh
@@ -46,11 +46,11 @@ install_prereqs() {
         if [[ "$ssl_maj_ver" -lt 3 ]]; then
             echo -e "***** linux/setup.sh - apt-get install with excluding SSL pkgs"
             apt-get -y update
-            apt-get -y install gcc g++ debian-builder rpm libffi-dev sqlite3 libsqlite3-dev wget alien  --allow-unauthenticated
+            apt-get -y install gcc g++ rpm libffi-dev sqlite3 libsqlite3-dev wget alien  --allow-unauthenticated
         else
             echo -e "***** linux/setup.sh - apt-get install with SSL pkgs"
             apt-get -y update
-            apt-get -y install gcc g++ zlib1g-dev openssl libssl-dev debian-builder rpm libffi-dev sqlite3 libsqlite3-dev wget alien  --allow-unauthenticated
+            apt-get -y install gcc g++ zlib1g-dev openssl libssl-dev rpm libffi-dev sqlite3 libsqlite3-dev wget alien  --allow-unauthenticated
         fi
 
     elif [ "$distro" == "CentOS" ] || [ "$distro" == "RHEL" ] || [ "$distro" == "Oracle" ] || [ "$distro" == "CloudLinux" ]; then
@@ -137,7 +137,7 @@ install_prereqs() {
 
     elif [ "$distro" == "Raspbian" ]; then
 
-        apt-get install gcc openssl sqlite3 libsqlite3-dev libffi-dev rpm git debian-builder alien libssl-dev -y
+        apt-get install gcc openssl sqlite3 libsqlite3-dev libffi-dev rpm git alien libssl-dev -y
 
     else
 
